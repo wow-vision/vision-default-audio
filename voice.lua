f1851749efd01381bffed07eb26e82cb6a1910de
Bradley Renshaw <bradjrenshaw@gmail.com>
2025-09-20 14:16:36 -0400

    Add Joanna voice pack

--- voice.lua
+++ voice.lua
@@ -3,3 +3,9 @@ WowVision.audio:registerVoicePack({
     label = "Matthew",
     filePath = "Interface/AddOns/WowVision/audio/voice/Matthew"
 })
+
+WowVision.audio:registerVoicePack({
+    key = "Joanna",
+    label = "Joanna",
+    filePath = "Interface/AddOns/WowVision/audio/voice/Joanna"
+})
