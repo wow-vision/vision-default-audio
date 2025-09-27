14a5a115f7adbd11d2c3f5e661c5c33450c6c4d5
Bradley Renshaw <bradjrenshaw@gmail.com>
2025-09-26 21:33:23 -0400

    code formatting via stylua

--- sound.lua
+++ sound.lua
@@ -3,14 +3,14 @@ local soundPath = "Interface/AddOns/WowVision/audio/sound"
 local pack = WowVision.audio.AudioPack:new({
     key = "WowVision",
     label = "WowVision",
-    filePath = soundPath
+    filePath = soundPath,
 })
 
 local root = pack:getDirectory()
 
 local alerts = root:addSubdirectory({
     key = "alerts",
-    label = "alerts"
+    label = "alerts",
 })
 
 alerts:addFiles({
@@ -27,20 +27,20 @@ alerts:addFiles({
     "glass4.mp3",
     "glass5.mp3",
     "start_small.mp3",
-    "stop_small.mp3"
+    "stop_small.mp3",
 })
 
-for i=1,27 do
+for i = 1, 27 do
     alerts:addFile("notification" .. i .. ".mp3")
 end
 
 local errors = root:addSubdirectory({
     key = "errors",
-    label = "errors"
+    label = "errors",
 })
 
 errors:addFiles({
-        "brang.mp3",
+    "brang.mp3",
     "bring.mp3",
     "dang.mp3",
     "drmm.mp3",
--- voice.lua
+++ voice.lua
@@ -1,11 +1,11 @@
 WowVision.audio:registerVoicePack({
     key = "Matthew",
     label = "Matthew",
-    filePath = "Interface/AddOns/WowVision/audio/voice/Matthew"
+    filePath = "Interface/AddOns/WowVision/audio/voice/Matthew",
 })
 
 WowVision.audio:registerVoicePack({
     key = "Joanna",
     label = "Joanna",
-    filePath = "Interface/AddOns/WowVision/audio/voice/Joanna"
+    filePath = "Interface/AddOns/WowVision/audio/voice/Joanna",
 })
